7451b504ea400b68252857e92c0f15f8f291552e
Adam Hundley <adamhundley@gmail.com>
2019-10-30 12:34:05 -0600

    update the single articles to handle stock recos!

--- single-article.php
+++ single-article.php
@@ -15,12 +15,46 @@ get_header();
     <section id="primary" class="article">
       <?php
         global $post;
+
+        $article_type = '';
+        $article_type_terms = get_the_terms($post, 'article-type');
+        if (isset($article_type_terms[0])) {
+            $article_type = $article_type_terms[0]->slug;
+        }
+
+        $article_ticker = '';
+        $article_ticker_terms = get_the_terms($post, 'article-ticker');
+        if (isset($article_ticker_terms[0])) {
+            $article_ticker = $article_ticker_terms[0]->slug;
+        }
         echo $post->post_title;
         echo get_the_date('Y-m-d', $post);
-        echo get_post_meta($post->ID, 'article_ticker', true);
+        echo $article_ticker;
         $user = get_userdata($post->post_author);
         echo $user->data->display_name;
         echo $post->post_content;
+        if ($article_type === 'stock-recommendations' && $article_ticker) {
+            $api_url = "https://financialmodelingprep.com/api/v3/company/profile/{$article_ticker}";
+            $response = wp_remote_get($api_url);
+
+            $response_code = wp_remote_retrieve_response_code($response);
+            if ($response_code === 200) {
+                $company = json_decode(wp_remote_retrieve_body($response));
+            } else {
+                throw new Exception("");
+            }
+
+            if ($company->profile) {
+                echo $company->profile->image;
+                echo $company->profile->companyName;
+                echo $company->profile->exchange;
+                echo $company->profile->description;
+                echo $company->profile->industry;
+                echo $company->profile->sector;
+                echo $company->profile->ceo;
+                echo $company->profile->website;
+            }
+        }
       ?>
     </section>
   </main>
