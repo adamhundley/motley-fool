20d17ea65fd8d253f2fdec9a2333b5d4d1f47408
Adam Hundley <adamhundley@gmail.com>
2019-10-30 16:31:17 -0600

    just a basic all articles tempalte

--- index.php
+++ index.php
@@ -1,27 +1,34 @@
 <?php
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
+ * The main file
  */
+
+require_once(__DIR__ . '/MotleyFool/Article.php');
+use MotleyFool\Article;
 
 get_header();
 ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
-			BALLS
-		</main><!-- .site-main -->
-	</section><!-- .content-area -->
+  <section id="primary" class="content-area">
+    <main id="main" class="article-wrapper container-fluid">
+      <div class="col">
+        <h1>All Articles</h1>
+        <?php
+          $posts = get_posts([
+              'numberposts' => -1,
+              'post_status' => 'publish',
+              'post_type' => [ 'article' ],
+              'orderby' => 'date',
+          ]);
+
+          foreach ($posts as $post) {
+              $article = new Article($post);
+              echo "<li class='list-group-item'><a href='{$article->getLink()}'>{$article->getTitle()} ({$article->getDisplayTicker()})</a></li>";
+          }
+        ?>
+      </div>
+    </main>
+  </section>
 
 <?php
 get_footer();
